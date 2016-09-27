edb3f919962fe3f40bac506629deb1a9dd4ed5ff
Carlos Tuñón <camotubi@gmail.com>
2016-09-26 23:48:20 -0500

    delete main,rename Gui to main

--- interfaces/Main.java
+++ interfaces/Main.java
@@ -1,45 +1,50 @@
 package interfaces;
 
 import java.awt.EventQueue;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import times.Timer;
-import interfaces.Gui;
-
-
-
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main {
 
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		Timer timer = new Timer();
-		
-		Gui window = new Gui();
-		
-		
-		try {
-			reader.read();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		timer.reset(3600000*12);
-		timer.start();
-		while(timer.getremainingTimeMillis()>0)
-		{
-			window.timerLabel.setText(timer.getRemHours()+":"+timer.getRemMinutes()+":"+""+timer.getRemSeconds()+":"+timer.getRemMilliSeconds());
-			//System.out.println(timer.getremainingTimeMillis());
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		window.timerLabel.setText(""+timer.getRemSeconds());
-		System.out.println("DAFAQ"+timer.getremainingTimeMillis());
-		
-		
-	}
+	private JFrame frame;
+	JLabel timerLabel = new JLabel("");
 
+	public Main() {
+		initialize();
+		frame.setVisible(true);
+	}
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		
+		timerLabel.setBounds(145, 43, 134, 57);
+		frame.getContentPane().add(timerLabel);
+		
+		JButton btnStart = new JButton("New button");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnStart.setBounds(105, 125, 89, 23);
+		frame.getContentPane().add(btnStart);
+		
+		JButton btnPause = new JButton("New button");
+		btnPause.setBounds(204, 125, 89, 23);
+		frame.getContentPane().add(btnPause);
+		
+		JButton btnStop = new JButton("New button");
+		btnStop.setBounds(303, 125, 89, 23);
+		frame.getContentPane().add(btnStop);
+		
+		JButton btnReset = new JButton("New button");
+		btnReset.setBounds(171, 159, 89, 23);
+		frame.getContentPane().add(btnReset);
+	}
 }
